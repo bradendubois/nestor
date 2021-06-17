598e1568e6ba1d14cd09326a12f704d2f8190a35
Braden Dubois <braden.dubois@usask.ca>
2021-06-16 19:27:30 -0600

    add methods to set bits in flag register

--- src/nestor/cpu/registers/mod.rs
+++ src/nestor/cpu/registers/mod.rs
@@ -10,6 +10,7 @@ pub struct Registers {
     p: u8       // Status Register
 }
 
+#[allow(dead_code)]
 impl Registers {
 
     pub fn new() -> Registers {
@@ -20,6 +21,56 @@ impl Registers {
             y: 0,
             s: 0xFD,
             p: 0x34
+        }
+    }
+
+    /***** Negative - Bit 7 (0x80) *****/
+    pub fn set_negative(&mut self, enabled: bool) {
+        match enabled {
+            true  => self.p |=  0x80,
+            false => self.p &= !0x80
+        }
+    }
+
+    /// Overflow - Bit 6 (0x40)
+    pub fn set_overflow(&mut self, enabled: bool) {
+        match enabled {
+            true  => self.p |=  0x40,
+            false => self.p &= !0x40
+        }
+    }
+
+    /// TODO - "B Flag" on bits 5,4
+
+    /// Decimal - Bit 3 (0x08)
+    pub fn set_decimal(&mut self, enabled: bool) {
+        match enabled {
+            true  => self.p |=  0x08,
+            false => self.p &= !0x08
+        }
+    }
+
+    /// Interrupt - Bit 2 (0x04)
+    pub fn set_interrupt(&mut self, enabled: bool) {
+        match enabled {
+            true  => self.p |=  0x04,
+            false => self.p &= !0x04
+        }
+    }
+
+    /// Zero - Bit 1 (0x02)
+    pub fn set_zero(&mut self, enabled: bool) {
+        match enabled {
+            true  => self.p |=  0x02,
+            false => self.p &= !0x02
+        }
+    }
+
+    /// Carry - Bit 0 (0x01)
+    pub fn set_carry(&mut self, enabled: bool) {
+        match enabled {
+            true  => self.p |=  0x01,
+            false => self.p &= !0x01
         }
     }
 }
