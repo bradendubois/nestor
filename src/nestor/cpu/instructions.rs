4e61a37f898922fbb1147351f21f33f79ec251c5
Braden Dubois <braden.dubois@usask.ca>
2021-06-18 21:48:10 -0600

    reordered opcode order in comments to match comparison order

--- src/nestor/cpu/instructions.rs
+++ src/nestor/cpu/instructions.rs
@@ -432,7 +432,7 @@ impl CPU6502 {
 /// Official Opcodes
 impl CPU6502 {
 
-    /// 0x61, 0x65, 0x69, 0x6D, 0x71, 0x75, 0x79, 0x7D  - Add with Carry
+    /// 0x69, 0x65, 0x75, 0x6D, 0x7D, 0x79, 0x61, 0x71  - Add with Carry
     fn adc(&mut self, mode: OperandMode) -> u8 {
         let (value, cycles) = match mode {
             Immediate => (self.immediate(), 2),
@@ -472,7 +472,7 @@ impl CPU6502 {
         cycles
     }
 
-    /// 0x21, 0x25, 0x29, 0x2D, 0x31, 0x35, 0x39, 0x3D - Bitwise AND with Accumulator
+    /// 0x29, 0x25, 0x35, 0x2D, 0x3D, 0x39, 0x21, 0x31 - Bitwise AND with Accumulator
     fn and(&mut self, mode: OperandMode) -> u8 {
         let (value, cycles) = match mode {
             Immediate => (self.immediate(), 2),
@@ -511,7 +511,7 @@ impl CPU6502 {
         cycles
     }
 
-    /// 0x06, 0x0A, 0x0E, 0x16, 0x01E - Arithmetic Shift Left
+    /// 0x0A, 0x06, 0x16, 0x0E, 0x1E - Arithmetic Shift Left
     fn asl(&mut self, mode: OperandMode, do_ora: bool) -> u8 {
         let (result, cycles) = match mode {
             Accumulator => {
@@ -619,7 +619,7 @@ impl CPU6502 {
         }
     }
 
-    /// 0xC6, 0xCE, 0xD6, 0xDE - Decrement Memory
+    /// 0xC6, 0xD6, 0xCE, 0xDE - Decrement Memory
     fn dec(&mut self, mode: OperandMode, do_cmp: bool) -> u8 {
         let (address, value, cycles) = match mode {
             ZeroPage => {
@@ -659,7 +659,7 @@ impl CPU6502 {
         cycles
     }
 
-    /// 0x41, 0x45, 0x49, 0x4D, 0x51, 0x55, 0x59, 0x5D - Bitwise Exclusive OR
+    /// 0x49, 0x45, 0x55, 0x4D, 0x5D, 0x59, 0x41, 0x51 - Bitwise Exclusive OR
     fn eor(&mut self, mode: OperandMode) -> u8 {
         let (value, cycles) = match mode {
             Immediate => (self.immediate(), 2),
@@ -698,7 +698,7 @@ impl CPU6502 {
         cycles
     }
 
-    /// 0xE6, 0xEE, 0xF6, 0xFE - Increment Memory
+    /// 0xE6, 0xF6, 0xEE, 0xFE - Increment Memory
     fn inc(&mut self, mode: OperandMode, do_sbc: bool) -> u8 {
         let (address, value, cycles) = match mode {
             ZeroPage => {
@@ -766,7 +766,7 @@ impl CPU6502 {
         6
     }
 
-    /// 0x46, 0x4E, 0x4A, 0x56, 0x5E - Logical Shift Right
+    /// 0x4A, 0x46, 0x56, 0x4E, 0x5E - Logical Shift Right
     fn lsr(&mut self, mode: OperandMode, do_eor: bool) -> u8 {
         let (result, cycles) = match mode {
             Accumulator => {
@@ -829,7 +829,7 @@ impl CPU6502 {
         2
     }
 
-    /// 0x01, 0x05, 0x09, 0x0D, 0x11, 0x15, 0x19, 0x1D - Bitwise OR with Accumulator
+    /// 0x09, 0x05, 0x15, 0x0D, 0x1D, 0x19, 0x01, 0x11 - Bitwise OR with Accumulator
     fn ora(&mut self, mode: OperandMode) -> u8 {
         let (value, cycles) = match mode {
             Immediate => (self.immediate(), 2),
@@ -868,7 +868,7 @@ impl CPU6502 {
         cycles
     }
 
-    /// 0x26, 0x2A, 0x2E, 0x36, 0x3E - Rotate Left
+    /// 0x2A, 0x26, 0x36, 0x2E, 0x3E - Rotate Left
     fn rol(&mut self, mode: OperandMode, do_and: bool) -> u8 {
         let (result, cycles) = match mode {
             Accumulator => {
@@ -926,7 +926,7 @@ impl CPU6502 {
         cycles
     }
 
-    /// 0x66, 0x6A, 0x6E, 0x76, 0x7E - Rotate Right
+    /// 0x6A, 0x66, 0x76, 0x6E, 0x7E - Rotate Right
     fn ror(&mut self, mode: OperandMode, do_adc: bool) -> u8 {
         let (result, cycles) = match mode {
             Accumulator => {
@@ -999,7 +999,7 @@ impl CPU6502 {
         6
     }
 
-    /// 0xE1, 0xE5, 0xE9, 0xED, 0xF1, 0xF5, 0xF9, 0xFD - Subtract with Carry
+    /// 0xE9, 0xE5, 0xF5, 0xED, 0xFD, 0xF9, 0xE1, 0xF1 - Subtract with Carry
     fn sbc(&mut self, mode: OperandMode) -> u8 {
         let (value, cycles) = match mode {
             Immediate => (self.immediate(), 2),
@@ -1147,7 +1147,7 @@ impl CPU6502 {
 
     /***** Load *****/
 
-    /// 0xA1, 0xA5, 0xA9, 0xAD, 0xB1, 0xB5, 0xB9, 0xBD - Load Accumulator
+    /// 0xA9, 0xA5, 0xB5, 0xAD, 0xBD, 0xB9, 0xA1, 0xB1 - Load Accumulator
     fn lda(&mut self, mode: OperandMode) -> u8 {
         match mode {
             Immediate | ZeroPage | ZeroPageX | ZeroPageY | Absolute | AbsoluteX | AbsoluteY | IndirectX | IndirectY => {
@@ -1159,7 +1159,7 @@ impl CPU6502 {
         }
     }
 
-    /// 0xA2, 0xA6, 0xAE, 0xB6, 0xBE - Load X
+    /// 0xA2, 0xA6, 0xB6, 0xAE, 0xBE - Load X
     fn ldx(&mut self, mode: OperandMode) -> u8 {
         match mode {
             Immediate | ZeroPage | ZeroPageY | Absolute | AbsoluteY => {
@@ -1171,7 +1171,7 @@ impl CPU6502 {
         }
     }
 
-    /// 0xA0, 0xA4, 0xAC, 0xB4, 0xBC - Load Y
+    /// 0xA0, 0xA4, 0xB4, 0xAC, 0xBC - Load Y
     fn ldy(&mut self, mode: OperandMode) -> u8 {
         match mode {
             Immediate | ZeroPage | ZeroPageX | Absolute | AbsoluteX => {
@@ -1185,7 +1185,7 @@ impl CPU6502 {
 
     /***** Store *****/
 
-    /// 0x81, 0x85, 0x8D, 0x91, 0x96, 0x99, 0x9D - Store Accumulator
+    /// 0x85, 0x95, 0x8D, 0x9D, 0x99, 0x81, 0x91 - Store Accumulator
     fn sta(&mut self, mode: OperandMode) -> u8 {
         match mode {
             ZeroPage | ZeroPageX | Absolute | AbsoluteX | AbsoluteY | IndirectX | IndirectY => self.store(mode, self.registers.a),
@@ -1193,7 +1193,7 @@ impl CPU6502 {
         }
     }
 
-    /// 0x86, 0x8E, 0x96 - Store X Register
+    /// 0x86, 0x96, 0x8E - Store X Register
     fn stx(&mut self, mode: OperandMode) -> u8 {
         match mode {
             ZeroPage | ZeroPageY | Absolute => self.store(mode, self.registers.x),
@@ -1201,7 +1201,7 @@ impl CPU6502 {
         }
     }
 
-    /// 0x84, 0x8C, 0x94 - Store Y Register
+    /// 0x84, 0x94, 0x8C - Store Y Register
     fn sty(&mut self, mode: OperandMode) -> u8 {
         match mode {
             ZeroPage | ZeroPageX | Absolute => self.store(mode, self.registers.y),
@@ -1332,12 +1332,12 @@ impl CPU6502 {
         self.sbc(Immediate)
     }
 
-    /// 0xE3, 0xE7, 0xEF, 0xF3, 0xF7, 0xFB, 0xFF - ISC: INC operand + SBC operand
+    /// 0xE7, 0xF7, 0xEF, 0xFF, 0xFB, 0xE3, 0xF3 - ISC: INC operand + SBC operand
     fn isc(&mut self, mode: OperandMode) -> u8 {
         self.inc(mode, true)
     }
 
-    /// 0xC3, 0xC7, 0xCF, 0xD3, 0xD7, 0xDB, 0xDF - DCP: DEC operand + CMP operand
+    /// 0xC7, 0xD7, 0xDF, 0xCF, 0xDB, 0xC3, 0xD3 - DCP: DEC operand + CMP operand
     fn dcp(&mut self, mode: OperandMode) -> u8 {
         self.dec(mode, true)
     }
