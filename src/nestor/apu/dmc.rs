0499bd329805b8a2d5e37bc61a7af2ac3bf95397
Braden Dubois <braden.dubois@usask.ca>
2021-07-05 02:19:59 -0600

    flesh dmc, noise

--- src/nestor/apu/dmc.rs
+++ src/nestor/apu/dmc.rs
@@ -1,13 +1,22 @@
 use crate::nestor::traits::MemoryMap;
+use super::APU;
 
+
+#[allow(dead_code)]
 pub struct DMC {
 
-    // DMC Channel
-    dmc_irq_main: u8,               // 0x4010
-    dmc_direct: u8,                 // 0x4011
-    dmc_sample_address: u8,         // 0x4012
-    dmc_sample_length_register: u8, // 0x4013
-    dmc_sample_length: u8,
+    // 0x4010
+    r_4010: u8,
+    irq_enable: bool,
+    loop_enable: bool,
+    frequency: u8,
+
+    load_counter: u8,       // 0x4011
+    sample_address: u8,     // 0x4012
+
+    // 0x4013
+    r_4013: u8,
+    sample_length: u8,
 
     pub bytes_remaining: u8,
     pub interrupt: bool,
@@ -17,12 +26,14 @@ impl DMC {
 
     pub fn new() -> DMC {
         DMC {
-            dmc_irq_main: 0,
-            dmc_direct: 0,
-            dmc_sample_address: 0,
-            dmc_sample_length_register: 0,
-            dmc_sample_length: 0,
-
+            r_4010: 0,
+            irq_enable: false,
+            loop_enable: false,
+            frequency: 0,
+            load_counter: 0,
+            sample_address: 0,
+            r_4013: 0,
+            sample_length: 0,
             bytes_remaining: 0,
             interrupt: false
         }
@@ -33,30 +44,29 @@ impl DMC {
 impl MemoryMap for DMC {
 
     fn read(&self, address: u16) -> u8 {
-        todo!()
+        match address {
+            0x4010 => self.r_4010,
+            0x4011 => self.load_counter,
+            0x4012 => self.sample_address,
+            0x4013 => self.r_4013,
+
+            _ => panic!("unmapped DMC address: {:#06X}", address)
+        }
     }
 
     fn write(&mut self, address: u16, value: u8) {
-
         match address {
             0x4010 => {
-                // self.dmc.r_4010 = value;
-                // self.dmc.irq_enable = value & 0x80 != 0;
-                // self.dmc.loop = value & 0x40 != 0;
-                // self.dmc.frequency = value & 0x0F;
+                self.r_4010 = value;
+                self.irq_enable = value & 0x80 != 0;
+                self.loop_enable = value & 0x40 != 0;
+                self.frequency = value & 0x0F;
             }
-
-            0x4011 => {
-                // self.dmc.load_counter = value & 0x4F;
-            }
-
-            0x4012 => {
-                // self.dmc.sample_address = value;
-            }
-
+            0x4011 => self.load_counter = value & 0x4F,
+            0x4012 => self.sample_address = value,
             0x4013 => {
-                // self.dmc.r_4013 = value;
-                // self.dmc.sample_length = APU::length_table_lookup((value & 0xF8) >> 3);
+                self.r_4013 = value;
+                self.sample_length = APU::length_table_lookup((value & 0xF8) >> 3);
             }
 
             _ => panic!("unmapped DMC address: {:#06X}", address)
--- src/nestor/apu/noise.rs
+++ src/nestor/apu/noise.rs
@@ -1,7 +1,23 @@
 use crate::nestor::traits::MemoryMap;
+use super::APU;
+
 
 pub struct Noise {
-    pub length_counter: u8
+
+    // 0x400C
+    r_400c: u8,
+    envelop_halt: bool,
+    constant_volume: bool,
+    volume: u8,
+
+    // 0x400E
+    r_400e: u8,
+    loop_noise: bool,
+    noise_period: u8,
+
+    // 0x400F
+    r_400f: u8,
+    length_counter: u8
 }
 
 
@@ -9,24 +25,48 @@ impl Noise {
 
     pub fn new() -> Noise {
         Noise {
+            r_400c: 0,
+            envelop_halt: false,
+            constant_volume: false,
+            volume: 0,
+            r_400e: 0,
+            loop_noise: false,
+            noise_period: 0,
+            r_400f: 0,
             length_counter: 0
         }
     }
 }
 
+
 impl MemoryMap for Noise {
 
     fn read(&self, address: u16) -> u8 {
-        todo!()
+        match address {
+            0x400C => self.r_400c,
+            0x400E => self.r_400e,
+            0x400F => self.r_400f,
+
+            _ => panic!("unmapped Noise register: {:#06X}", address)
+        }
     }
 
     fn write(&mut self, address: u16, value: u8) {
         match address {
-            0x400C => self.noise_main = value,
-            0x400E => self.noise_loop_period = value,
+            0x400C => {
+                self.r_400c = value;
+                self.envelop_halt = value & 0x20 != 0;
+                self.constant_volume = value & 0x10 != 0;
+                self.volume = value & 0x0F;
+            }
+            0x400E => {
+                self.r_400e = value;
+                self.loop_noise = value & 0x80 != 0;
+                self.noise_period = value & 0x0F;
+            }
             0x400F => {
-                self.noise_length_register = value;
-                self.noise_length = APU::length_table_lookup((value & 0xF8) >> 3);
+                self.r_400f = value;
+                self.length_counter = APU::length_table_lookup((value & 0xF8) >> 3);
             },
 
             _ => panic!("unmapped Noise register: {:#06X}", address)
